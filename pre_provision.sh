bc1bd2671ab5cd66c74fc1d0cbe85cd89b54c82c
JohnVonNeumann <louiswillcock@gmail.com>
2018-02-17 22:20:19 +1100

    update the pre_prov shell script, new knowledge has shown a cleaner and more secure way of doing stuff, untested on a fresh distro however, so results could potentially be tainted, still getting failures on ssh related roles, due to obvious non-sudo running, can fix

--- pre_provision.sh
+++ pre_provision.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 sudo apt install python-pip
 pip install --upgrade pip
-sudo pip install ansible
+pip install --user virtualenv
+virtualenv venv-27
+source venv-27/bin/activate
+pip install ansible
+ansible-playbook provision_me_like_one_of_your_french_girls.yml 
