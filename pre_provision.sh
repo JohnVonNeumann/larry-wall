e0b3f58ab1751f58705c1fc9e589fb13058271bd
JohnVonNeumann <louiswillcock@gmail.com>
2018-02-18 11:51:04 +1100

    unclean version of the script but i got the apt installs working without installing via venvs, its at user-system level, using --user, need to better understand ramifications of thi, woke up and had the idea lol

--- pre_provision.sh
+++ pre_provision.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-sudo apt install python-pip python3-pip -y
-sudo add-apt-repository ppa:jonathonf/python-3.6 -y
 sudo apt-get update
-sudo apt-get install python3.6 -y
+sudo apt install python-pip python3-pip -y
+#sudo add-apt-repository ppa:jonathonf/python-3.6 -y
+#sudo apt-get install python3.6 -y
 pip install --upgrade pip
 pip install --user virtualenv
-virtualenv -p python3.6 venv-36
-source venv-36/bin/activate
-pip install ansible
+#virtualenv -p python3.6 venv-36
+#source venv-36/bin/activate
+pip install --user ansible
 ansible-playbook provision_me_like_one_of_your_french_girls.yml 
