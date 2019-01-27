9ae1c82d05ddc0b7d90fb46d923897368eb9474d
JohnVonNeumann <louiswillcock@gmail.com>
2019-01-27 14:26:01 +1100

    UPDATE local_pre_prov.sh - fix prov script

--- local_pre_provision.sh
+++ local_pre_provision.sh
@@ -7,5 +7,6 @@ pip install --upgrade pip
 pip install --user virtualenv
 #virtualenv -p python3.6 venv-36
 #source venv-36/bin/activate
-pip install --user ansible
+# molecule has been installed via brew on macos
+pip install --user ansible molecule
 ansible-playbook provision_me_like_one_of_your_french_girls.yml 
