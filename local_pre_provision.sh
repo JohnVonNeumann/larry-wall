8186336e42e1efcdb90b634b9b8dbbec6f677bba
JohnVonNeumann <louiswillcock@gmail.com>
2019-06-26 20:22:14 +1000

    UPDATE lpp.sh - fix local pre-provision script for ubu 18.04

--- local_pre_provision.sh
+++ local_pre_provision.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt install python-pip python3-pip -y
-#sudo add-apt-repository ppa:jonathonf/python-3.6 -y
-#sudo apt-get install python3.6 -y
-pip install --upgrade pip
-pip install --user virtualenv
-#virtualenv -p python3.6 venv-36
-#source venv-36/bin/activate
-pip install --user ansible
-ansible-playbook provision_me_like_one_of_your_french_girls.yml 
+sudo apt install python3-pip -y
+pip3 install --user ansible
+~/.local/bin/ansible-playbook provision_me_like_one_of_your_french_girls.yml --ask-become-pass
